7910910a42f65a0fe25875740d0f19fae6a1d0d5
marikoja <Marikoanderson@gmail.com>
2018-05-25 15:39:06 -0700

    wave 1 complete, hard coded data renders on page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,44 @@ import './App.css';
 import timelineData from './data/timeline.json';
 
 import Timeline from './components/Timeline';
+import Timestamp from './components/Timestamp';
+
+
 
 class App extends Component {
   render() {
     console.log(timelineData);
 
-    // Customize the code below
+    const posts = [
+      {
+        person: "Maggie Mutt",
+        status: "Guess what I smelled today!",
+        timestamp: "2018-05-18T22:19:40Z"
+      }
+    ]
+
+    const postsComponents = posts.map((post) => {
+      return (
+        <li key={post.person}>
+          <p>{post.person}</p>
+          <p>{post.status}</p>
+          <p><Timestamp time={post.timestamp}/></p>
+        </li>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Really Realistic Life Posts</h1>
         </header>
         <main className="App-main">
+          {postsComponents}
         </main>
       </div>
     );
+
+
   }
 }
 
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
-    // Fill in your code here
-    return;
+    const person = this.props.person;
+    const status = this.props.status
+    const timestamp = this.props.timestamp;
+
+    return(
+      <article>
+        <h3>Person: {person}</h3>
+        <p>Status: {status}</p>
+        <p>Time: {timestamp} </p>
+      </article>
+    );
   }
 }
 
