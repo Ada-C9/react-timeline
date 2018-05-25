97f09acfe9055a7657b61696f7360cfddbe42076
LetiTran <leticiastran@gmail.com>
2018-05-25 14:46:58 -0700

    implemented the render function of TimelineEvent using data from JSON and Timestamp.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,37 @@ import './App.css';
 import timelineData from './data/timeline.json';
 
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 class App extends Component {
   render() {
     console.log(timelineData);
 
+
+    const eventComponents = timelineData.events.map((event) => {
+      return(
+        <li key={event.person}>
+        <TimelineEvent
+        person={event.person}
+        status={event.status}
+        time={event.timeStamp}
+        />
+        </li>
+      );
+    });
+
     // Customize the code below
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Application title</h1>
-        </header>
-        <main className="App-main">
-        </main>
+      <header className="App-header">
+      <h1 className="App-title">Application title</h1>
+      </header>
+      <main className="App-main">
+
+      {eventComponents}
+
+
+      </main>
       </div>
     );
   }
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
-    // Fill in your code here
-    return;
+    const person = this.props.person;
+    const status = this.props.status;
+    // const time = <Timestamp time={Date.now()}/>
+    const time = this.props.time;
+    return(
+      <article>
+      <h3>Name: {person}</h3>
+      <p>status: {status}</p>
+      <p>time: {time}</p>
+      </article>
+    );
   }
 }
 
