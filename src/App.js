491c4441ab28807d632cab9525da6cde57e5b5dc
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-25 16:34:40 -0700

    fixed big with title and with width

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import TimelineEvent from './components/TimelineEvent';
+// import TimelineEvent from './components/TimelineEvent';
 
 class App extends Component {
   render() {
-    // const currTimelineEvents = timelineData['events'];
     const timelineEventsList = <Timeline eventsList={timelineData['events']}/>;
+    const pageTitle = timelineData['person'] + '\'s Social Media Feed';
 
     console.log(timelineData);
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">{pageTitle}</h1>
         </header>
         <main className="App-main">
         {timelineEventsList}
