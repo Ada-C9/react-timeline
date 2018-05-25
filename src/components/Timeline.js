731a290cc4a3db9775502d910afa2cce0514a62e
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-25 15:48:42 -0700

    add timeline event componant and render in timeline component.  Render timeline component in app.  Can view single timeline event

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,10 +3,14 @@ import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
 class Timeline extends React.Component {
-  render() {
-    // Fill in your code here
-    return;
-  }
+	render() {
+		// Fill in your code here
+		return(
+			<section>
+				<TimelineEvent person="Adele Goldberg" status="In Smalltalk, everything happens somewhere else." timeStamp= "2018-05-18T22:12:03Z" />
+			</section>
+		);
+	}
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,10 +3,20 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
 class TimelineEvent extends React.Component {
-  render() {
-    // Fill in your code here
-    return;
-  }
+
+	render() {
+		const person = this.props.person || 'no person';
+		const status = this.props.status || 'no status';
+		const timeStamp = this.props.timestamp || 'no timestamp';
+		// Fill in your code here
+		return (
+			<section className='timeline-event'>
+				<div className='event-person'>{ person }</div>
+				<div className='event-status'>{ status }</div>
+				<div className='event-time'>{ timeStamp }</div>
+			</section>
+		);
+	}
 }
 
 export default TimelineEvent;
