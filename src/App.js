77160543f3d9c4e5b876f0e85a8056ab6bb0c819
Hannah Cameron <hannahiscoding@gmail.com>
2018-05-25 16:07:14 -0700

    passing wave 1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import timelineData from './data/timeline.json';
 
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 class App extends Component {
   render() {
     console.log(timelineData);
+
+    const testEvents = [
+      {
+        person: "Ada",
+        status: "So stoked - an amazing organization just named itself after me!",
+        timestamp: "2018-05-16T22:12:03Z"
+      },
+      {
+        person: "Grace",
+        status: "Props to @Ada - so happy for you! Hopefully someone in the future will do the same for me, but TBH it probably won't be quite as good.",
+        timestamp: "2018-05-16T22:17:03Z"
+      }
+    ];
+
+    const eventComponents = testEvents.map((event, index) => {
+      return <TimelineEvent key={index} person={event.person} status={event.status} timestamp= {event.timestamp}/>
+    })
 
     // Customize the code below
     return (
@@ -15,6 +33,7 @@ class App extends Component {
           <h1 className="App-title">Application title</h1>
         </header>
         <main className="App-main">
+        {eventComponents}
         </main>
       </div>
     );
