3fb3ddf7f14613910c548c8188cb4340f6a4f0f2
Nora Peters <norapeters5@gmail.com>
2018-05-26 13:21:51 -0700

    adds timelinedata.person as name in app title, adds timeline jsx. playing around with state using a counter in footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-
 import Timeline from './components/Timeline';
+import Counter from './components/Counter';
+
 
 class App extends Component {
   render() {
-    console.log(timelineData);
+    const name = timelineData.person
 
-    // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">{name}</h1>
         </header>
         <main className="App-main">
+          <section>
+            <Timeline timeline={timelineData} />
+          </section>
         </main>
+        <footer>
+          <small><Counter/></small>
+        </footer>
       </div>
     );
   }
