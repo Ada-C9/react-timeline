4c5db633de255169eab9e6c93176d38cc7d8dffb
Tor Shimizu <torshimizu@gmail.com>
2018-05-26 10:34:00 -0700

    escaped apostrophe

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Ada's Feed</h1>
+          <h1 className="App-title">Ada&#39;s Feed</h1>
         </header>
         <main className="App-main">
           <section id='app'>
