986b1ffa13b3e4c1cd0f1b3a30471c98a0d404f9
Anne <annehwatson@gmail.com>
2018-05-26 10:28:53 -0700

    timestamp p correct now

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,18 +2,14 @@ import React, { Component } from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-//something is passed from App to Timeline (JSON??) that will render a list of TimelineEvent components
-//wave2 will pass in the json data
-//apply the pre-built css classes to the html
-
 class Timeline extends React.Component {
   render() {
     let eventData = this.props.events
 
     return (
-      <section className="timeline">
+      <div className="timeline">
         {eventData}
-      </section>
+      </div>
     );
   }
 }
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,26 +2,23 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-//individual Timeline Events
-//person, status message, timestamp (already a component)
-//wave 1 just put in hardcoded data
-
 class TimelineEvent extends React.Component {
   render() {
     let person = this.props.person
     let status = this.props.status
-    let timeStamp = ( < Timestamp />)
+    let timeStamp = this.props.timeStamp
 
 
     return (
       <section className="timeline-event">
-        <strong className="event-person">{ person }</strong>
-        <br/>
-        <strong className="event-status">{ status}</strong>
-        <br/>
-        <strong className="event-time">{ timeStamp }</strong>
+        <div className="event-person">{ person }</div>
 
-        <br/>
+        <div className="event-status">{ status}</div>
+
+        <div className="event-time"><Timestamp time={ timeStamp }></Timestamp>
+        </div>
+
+
       </section>
     );
   }
