9b18a713ead7ce94eb12a920ae58cd6450b26b8f
Wenjie <traxiaojie@gmail.com>
2018-05-25 21:30:31 -0700

    Added return block to Timeline, using TimelineEvent

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+// import timelineData from './data/timeline.json';
 
-class Timeline extends React.Component {
-  render() {
-    // Fill in your code here
-    return;
-  }
+class Timeline extends Component {
+	render() {
+
+		let data = this.props.data;
+
+		const allData = data.map( (info, index) => {
+			return <TimelineEvent key={ index } person={ info.person } status={ info.status } time={ info.time }/>;
+		});
+
+		return (
+			<div className="timeline-container"> { allData }
+			</div>
+		);
+	}
 }
 
 export default Timeline;
