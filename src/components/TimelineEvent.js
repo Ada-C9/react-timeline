e87975908123e66f201bdfbd566e66e0c995b530
Nora Peters <norapeters5@gmail.com>
2018-05-26 14:56:31 -0700

    creates TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
-    // Fill in your code here
-    return;
+    const { person, status, timestamp } = this.props
+
+    return (
+      <article className="timeline-event">
+        <div className="event-person">
+          {person}
+        </div>
+        <div className="event-time">
+          <Timestamp time={timestamp} />
+        </div>
+        <p className="event-status">
+          {status}
+        </p>
+      </article>
+    );
   }
 }
 
