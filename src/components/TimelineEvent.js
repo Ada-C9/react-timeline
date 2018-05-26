e45af80769552897fc079cab46e33b454bf790de
Wenjie <traxiaojie@gmail.com>
2018-05-25 21:29:20 -0700

    Added TimelineEvent js file

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
     // Fill in your code here
-    return;
+		let person = this.props.person;
+    let status = this.props.status;
+		let time = < Timestamp />;
+
+    return(
+			<section className="event">
+			  <div className="top">
+          <span>{ person }</span>
+					<span className="time-stamp">{ time }</span>
+				</div>
+				<div className="bottom">
+          { status }
+				</div>
+			</section>
+		);
   }
 }
 
