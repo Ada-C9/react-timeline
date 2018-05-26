a3e418ce71d412f24377e4450afca90d7880464f
Ari <dominiquearia0@gmail.com>
2018-05-26 10:17:42 -0700

    imported TimelineEvent, map user, status, and time to seed data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-
+import TimelineEvent from './components/TimelineEvent';
 import Timeline from './components/Timeline';
 
 class App extends Component {
   render() {
-    console.log(timelineData);
+    const seedData = timelineData.events
+    const feedData = seedData.map((user, index) => {
+      return (<TimelineEvent key={index} person={ user.person } status={user.status} posted={user.timeStamp} />)
+    });
 
-    // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Ada social media page</h1>
         </header>
         <main className="App-main">
+          { feedData }
         </main>
       </div>
     );
