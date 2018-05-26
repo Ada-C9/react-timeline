b80bdea1af861516f3f107281df1a1a0deaa798d
Ari <dominiquearia0@gmail.com>
2018-05-26 10:18:36 -0700

    removed react.component from class

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
-    // Fill in your code here
-    return;
+    const name = this.props.person;
+    const message = this.props.status;
+    const posted = (<Timestamp time={this.props.timeStamp} />);
+    return (
+      <section>
+        <ul className="timeline-event">
+          <li className="event-person">{ name }</li>
+          <li className="event-time">{ posted }</li>
+          <li className="event-status">{ message }</li>
+        </ul>
+      </section>
+    );
   }
 }
 
