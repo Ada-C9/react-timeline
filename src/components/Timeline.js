93df134d5852223feeb5c2a1604a8721940ea8f6
Aruna <aruna.thungathurthi@gmail.com>
2018-05-25 17:08:00 -0700

    create timelineevent component to timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+import data from '../data/timeline.json';
 
 class Timeline extends React.Component {
   render() {
     // Fill in your code here
-    return;
+    const events = data.events;
+
+        const timelineEventcomponents = events.map((event) => {
+          return(
+            <TimelineEvent
+            key={event.timestamp}
+            person={event.person}
+            status={event.status}
+            timestamp ={event.timestamp}
+            />
+
+          );
+        });
+        return(
+          <div>{timelineEventcomponents}</div>
+        );
+    }
   }
-}
+
+
 
 export default Timeline;
