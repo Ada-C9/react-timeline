e5e0c95a9e243ae90e547506cec9b855918e49b8
Dikla <dikla.aharoni@gmail.com>
2018-05-26 10:22:36 -0700

    Created TimelineEvent props

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,7 +5,21 @@ import TimelineEvent from './TimelineEvent';
 class Timeline extends React.Component {
   render() {
     // Fill in your code here
-    return;
+    console.log(this.props.eventsData);
+
+    const events = this.props.eventsData;
+
+    const eventsComponents = events.map((event, index) => {
+      return <TimelineEvent key= {index} person= {event.person} status= {event.status} timeStamp= {event.timeStamp} />
+    });
+
+    return (
+      <div>
+        {eventsComponents}
+      </div>
+    );
+
+
   }
 }
 
