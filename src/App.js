c03cda16b4074f10e72629ae49a7498281844fc3
SelamawitA <Selamawit.ainalem@gmail.com>
2018-05-28 16:38:41 -0700

    whoops forgot commits on this one, sorry yall

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-
+import TimelineEvent from './components/TimelineEvent.js';
 import Timeline from './components/Timeline';
 
 class App extends Component {
   render() {
-    console.log(timelineData);
+    // console.log('TimelineData');
+    // console.log(timelineData.events);
+    // let data = timelineData["events"];
+    // console.log(data);
+let array_of_users = timelineData.events;
+//pass in array of events Here
+console.log(array_of_users)
+
+//
+// let array_of_users = timelineData.events;
+// console.log(array_of_users);
+
+  // const timelineData = data.map((a_post) => {
+  //     return (<TimelineEvent
+  //         key={a_post.timeStamp}
+  //         person= {a_post.person}
+  //         status= {a_post.status}
+  //         timeStamp= {a_post.timeStamp}/>
+  //       )
+  //   });
+//above it .map method
 
     // Customize the code below
     return (
@@ -16,6 +36,9 @@ class App extends Component {
         </header>
         <main className="App-main">
         </main>
+        <div>
+          < Timeline events={array_of_users} />
+        </div>
       </div>
     );
   }
