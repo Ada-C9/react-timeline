ebd2f62b5a6dcf3c5ed3cbe3803e751ca1b9273e
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-05-28 14:51:53 -0700

    fully working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-
 import Timeline from './components/Timeline';
 
 class App extends Component {
   render() {
     console.log(timelineData);
 
+    const person = timelineData["person"];
+    const events = timelineData["events"];
+
+    const timelineComponents = <Timeline person={ person } events={ events } />;
+
     // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">{`${person}'s Timeline`}</h1>
         </header>
         <main className="App-main">
+          { timelineComponents }
         </main>
       </div>
     );
