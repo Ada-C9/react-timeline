c76d24792f04449e00414040ab9bb16bdeced32b
kseastman <kat.eastman@gmail.com>
2018-05-27 20:13:22 -0700

    styling finished.

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-class TimelineEvent extends React.Component {
+class TimelineEvent extends Component {
   render() {
-    // Fill in your code here
-    return;
+    const person = this.props.person;
+    const status = this.props.status;
+    const timeStamp = this.props.timeStamp;
+
+    return (
+      <section className="post">
+        <h2 className="user-name">{person}</h2>
+        <article className="status">{status}</article>
+        <span className="timestamp"><Timestamp time={timeStamp} /></span>
+      </section>
+    );
   }
 }
 
