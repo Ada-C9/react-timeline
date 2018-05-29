088eeb4bbb5e04cda832028330700a1edea53bbb
Brenda Rios <riosbrenda@gmail.com>
2018-05-28 22:55:40 -0700

    Added props for person, status and timestamp to TimelineEvent.

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,7 +5,19 @@ import Timestamp from './Timestamp';
 class TimelineEvent extends React.Component {
   render() {
     // Fill in your code here
-    return;
+    const person = this.props.person;
+    const timeStamp = <Timestamp time={this.props.timeStamp}/>;
+    const status = this.props.status;
+
+    return (
+      <section>
+      <p>{person}<span>{timeStamp}</span></p>
+
+      <p>{status}</p>
+
+      </section>
+    );
+
   }
 }
 
