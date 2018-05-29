ebeeb28da55804e429a8ceeef0136d4c81040d4a
Abiaina <abinnet.a@gmail.com>
2018-05-29 00:03:29 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -9,42 +9,7 @@ class App extends Component {
 
   render() {
     console.log(timelineData);
-    //
-    // const posts = [
-    //     {
-    //       person: 'Harriet Potter',
-    //       status: 'i am more than anti-voldemort',
-    //       timestamp: Date.now
-    //     },
-    //     {
-    //       person: 'Ada',
-    //       status: 'coding for the future',
-    //       timestamp: Date.now
-    //     },
-    //
-    //     {
-    //       person: 'Hermonie',
-    //       status: 'time travelling by 14, saving the world again',
-    //       timestamp: Date.now
-    //     },
-    //   ];
-    //
-    //   const postComponents = posts.map((post) => {
-    //
-    //     return (
-    //       <li key={post.person}>
-    //
-    //         < TimelineEvent
-    //           person = {post.person}
-    //           status = {post.status}
-    //           timestamp = {post.timestamp}
-    //         />
-    //
-    //       </li>
-    //     );
-    //   });
-
-
+    
     // Customize the code below
     return (
       <div className="App">
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -7,9 +7,9 @@ class TimelineEvent extends React.Component {
     // Fill in your code here
     return(
       <div>
-        <h3>person: {this.props.person}</h3>
-        <p>status: {this.props.status}</p>
-        <p>timestamp: <Timestamp time = {this.props.timestamp}/></p>
+        <big> {this.props.person}</big>
+        <time id="rightSide">< Timestamp time = {this.props.timestamp}/></time>
+        <p>{this.props.status}</p>
       </div>
     );
   }
