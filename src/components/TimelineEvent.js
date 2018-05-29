d02decf9a9f9dda1d1d26cfe6211f370f14e308a
Brittany Jones <brittany.r.jones@hotmail.com>
2018-05-29 00:46:50 -0700

    json data loaded, and visible to page

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,15 +4,22 @@ import Timestamp from './Timestamp';
 
 class TimelineEvent extends React.Component {
   render() {
-    return (
-    <div>
-       <p>Brittany</p>
-       <p>Should be Sleeping</p>
-       <p><Timestamp /></p>
-       </div>
+    const person = this.props.person;
+    const status = this.props.status;
+    const timeStamp = this.props.timeStamp
 
-   );
- }
+
+    // Should take in 3 props, person, timeStamp and status
+    return (
+
+      <div>
+      <p>{person}</p>
+      <p>{status}</p>
+      <p>{timeStamp}</p>
+      </div>
+
+    );
+  }
 }
 
 
