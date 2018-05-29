7d13d985f0635395cc4b6a9c89dbf80e8ab17dca
Maja Graonic <m.graonic@gmail.com>
2018-05-28 20:59:04 -0700

    final changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,11 +6,12 @@ class TimelineEvent extends React.Component {
   render() {
     const person = this.props.person;
     const status = this.props.status;
+    const timeStamp = this.props.timeStamp;
 
     return(
         <article className="timeline-event">
           <span className="event-person"><strong>{person}</strong></span>
-          <span className="event-time"><Timestamp /></span>
+          <span className="event-time"><Timestamp time= { timeStamp }/></span>
           <p className="event-status">{status}</p>
         </article>
     )
