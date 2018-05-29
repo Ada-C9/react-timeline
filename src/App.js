49430e847cdffbd9f5ab85e33c0dc8a79a08c0d0
Steffany Brown <steffannbrown@gmail.com>
2018-05-29 09:04:00 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-import TimelineEvent from './components/TimelineEvent';
+
 import Timeline from './components/Timeline';
 
 class App extends Component {
   render() {
-    const testPosts = [
-      {
-        "person": "Adele Goldberg",
-        "status": "In Smalltalk, everything happens somewhere else.",
-        "timeStamp": "2018-05-18T22:12:03Z"
-      },
-      {
-        "person": "Erica Baker",
-        "status": "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
-        "timeStamp": "2018-05-18T22:19:40Z"
-      } ];
 
-      const thisTimeline = <Timeline events={testPosts} />
+      const thisTimeline = <Timeline events={timelineData.events} />
 
     console.log(timelineData);
 
-    // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">{timelineData.person}'s Timeline</h1>
         </header>
         <main className="App-main">
-        {thisTimeline}
+          {thisTimeline}
         </main>
       </div>
     );
